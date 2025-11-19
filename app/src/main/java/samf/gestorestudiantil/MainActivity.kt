bf3b7ee12ecce3e08a6a070925d1312dc7ec7dc9
menonisebastian <arturomenoni@gmail.com>
2025-11-20 00:08:13 +0100

    2

--- app/src/main/java/samf/gestorestudiantil/MainActivity.kt
+++ app/src/main/java/samf/gestorestudiantil/MainActivity.kt
@@ -3,16 +3,50 @@ package samf.gestorestudiantil
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.windowInsetsTopHeight
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Search
-import androidx.compose.material3.*
+import androidx.compose.material.icons.outlined.Info
+import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
 import samf.gestorestudiantil.ui.theme.GestorEstudiantilTheme
 
 
@@ -29,22 +63,102 @@ class MainActivity : ComponentActivity() {
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MyScaffoldScreen() {
-    Scaffold(
-        topBar = { MyTopAppBar() },
-        floatingActionButton = { MyFloatingActionButton() }
-    ) { innerPadding ->
-        // Contenido principal de la pantalla
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(innerPadding),
-            contentAlignment = Alignment.Center
-        ) {
-            Text(
-                text = "Aquí aparecerá tu lista de tareas",
-                style = MaterialTheme.typography.titleMedium
-            )
+fun MyScaffoldScreen()
+{
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
+
+    ModalNavigationDrawer(
+        drawerContent = {
+            ModalDrawerSheet {
+                Column(modifier = Modifier.fillMaxSize()) {
+                    Column(
+                        modifier = Modifier
+                            .weight(1f)
+                            .padding(horizontal = 16.dp)
+                            .verticalScroll(rememberScrollState())
+                    ) {
+                        Spacer(Modifier.height(12.dp))
+                        Text("Drawer Title", modifier = Modifier.padding(16.dp), style = MaterialTheme.typography.titleLarge)
+                        HorizontalDivider()
+
+                        Text("Section 1", modifier = Modifier.padding(16.dp), style = MaterialTheme.typography.titleMedium)
+                        NavigationDrawerItem(
+                            label = { Text("Item 1") },
+                            selected = false,
+                            onClick = { /* Handle click */ }
+                        )
+                        NavigationDrawerItem(
+                            label = { Text("Item 2") },
+                            selected = false,
+                            onClick = { /* Handle click */ }
+                        )
+
+                        HorizontalDivider(modifier = Modifier.padding(vertical = 8.dp))
+
+                        Text("Section 2", modifier = Modifier.padding(16.dp), style = MaterialTheme.typography.titleMedium)
+                        NavigationDrawerItem(
+                            label = { Text("Settings") },
+                            selected = false,
+                            icon = { Icon(Icons.Outlined.Settings, contentDescription = null) },
+                            badge = { Text("20") }, // Placeholder
+                            onClick = { /* Handle click */ }
+                        )
+                        NavigationDrawerItem(
+                            label = { Text("Help and feedback") },
+                            selected = false,
+                            icon = { Icon(Icons.Outlined.Info, contentDescription = null) },
+                            onClick = { /* Handle click */ },
+                        )
+                        Spacer(Modifier.height(12.dp))
+                    }
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(16.dp),
+                        contentAlignment = Alignment.Center
+                    ){
+                        Image(
+                            painter = painterResource(id = R.drawable.diario),
+                            modifier = Modifier
+                                .width(50.dp),
+                            contentDescription = "logo texto"
+                        )
+                    }
+                }
+            }
+        },
+        drawerState = drawerState
+    ){
+        Spacer(Modifier.windowInsetsTopHeight(WindowInsets.statusBars))
+        Scaffold(
+            topBar = {
+                MyTopAppBar(
+                    onMenuClick = {
+                        scope.launch {
+                            if (drawerState.isClosed) {
+                                drawerState.open()
+                            } else {
+                                drawerState.close()
+                            }
+                        }
+                    }
+                )
+                     },
+            floatingActionButton = { MyFloatingActionButton() }
+        ) { innerPadding ->
+            // Contenido principal de la pantalla
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(innerPadding),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = "Aquí aparecerá tu lista de tareas",
+                    style = MaterialTheme.typography.titleMedium
+                )
+            }
         }
     }
 }
@@ -54,7 +168,8 @@ fun MyFloatingActionButton() {
     FloatingActionButton(
         onClick = { /* Acción principal */ },
         containerColor = MaterialTheme.colorScheme.primary,
-        contentColor = MaterialTheme.colorScheme.onPrimary
+        contentColor = MaterialTheme.colorScheme.onPrimary,
+        shape = CircleShape
     ) {
         Icon(Icons.Default.Add, contentDescription = "Añadir elemento")
     }
@@ -62,11 +177,11 @@ fun MyFloatingActionButton() {
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MyTopAppBar() {
+fun MyTopAppBar(onMenuClick: () -> Unit = {}) {
     TopAppBar(
         title = { Text("Gestor Estudiantil") },
         navigationIcon = {
-            IconButton(onClick = { /* acción al pulsar menú */ }) {
+            IconButton(onClick = { onMenuClick() }) {
                 Icon(Icons.Default.Menu, contentDescription = "Menú")
             }
         },
@@ -78,7 +193,7 @@ fun MyTopAppBar() {
         colors = TopAppBarDefaults.topAppBarColors(
             containerColor = MaterialTheme.colorScheme.primary,
             titleContentColor = MaterialTheme.colorScheme.onPrimary,
-            navigationIconContentColor = MaterialTheme.colorScheme.onPrimary,
+navigationIconContentColor = MaterialTheme.colorScheme.onPrimary,
             actionIconContentColor = MaterialTheme.colorScheme.onPrimary
         )
     )
