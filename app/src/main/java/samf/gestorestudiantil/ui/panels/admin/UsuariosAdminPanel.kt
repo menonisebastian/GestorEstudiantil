ff8a8677f7080678e2e3d9f9ae4cdec780b7b526
menonisebastian <arturomenoni@gmail.com>
2026-02-23 23:20:40 +0100

    v0.1.2

--- app/src/main/java/samf/gestorestudiantil/ui/panels/admin/UsuariosAdminPanel.kt
+++ app/src/main/java/samf/gestorestudiantil/ui/panels/admin/UsuariosAdminPanel.kt
@@ -40,7 +40,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.retain.retain
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
